717d328686ecc8e3f4103ee4840f9abe5ed9e805
SlicedorDiced <Joshua.luna-perez@bryanuniversity.edu>
2022-05-12 15:17:23 -0400

    week4/5/6

--- week4/axios.js
+++ week4/axios.js
@@ -1,17 +1,35 @@
 const userName = document.querySelector('#name');
-const button = document.querySelector('#bttn').addEventListener('click', requestAxiosData)
+const desc = document.querySelector('#description');
+const price1 = document.querySelector('#price');
+const complition = document.querySelector('#iscom');
+const getbutton = document.querySelector('#getbttn').addEventListener('click', requestAxiosData);
+const  postbutton = document.querySelector('#postbttn').addEventListener('click', postAxiosData);
+
+function postAxiosData(){
+    axios.post(`http://api.bryanuniversity.edu/${userName.value}/list`,{
+        description : desc.value,
+        price: price1.value,
+        isComplete : false
+    })
+    
+}
+
 
 function requestAxiosData(){
-
     axios.get(`http://api.bryanuniversity.edu/${userName.value}/list`)
-.then(response => {
+        .then(response => displayData(response))
+        
+        .catch(error => console.log(error))
+}
+
+
+
+function displayData(response) {
     for(i = 0; i < response.data.length; i++){ 
         const h1 = document.createElement('h1')
         h1.textContent = response.data[i].name
         document.body.appendChild(h1)
     }
-})
-.catch(error => console.log(error))
 }
 
 
